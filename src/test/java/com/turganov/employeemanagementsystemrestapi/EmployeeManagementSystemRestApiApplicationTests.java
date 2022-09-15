84403153204e0d3d934fc0e9df9ba91cc13dfecc
Rustambek <aaa_ppp_1989@mail.ru>
2022-09-15 22:05:10 +0600

    first commit

---
+++ src/test/java/com/turganov/employeemanagementsystemrestapi/EmployeeManagementSystemRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turganov.employeemanagementsystemrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
